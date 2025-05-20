eb4a9ed2199497ca9ab71eb98a095913f7bcefc2
jalengaden <jdalengaden@gmail.com>
2025-05-19 19:44:47 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,32 +1,32 @@
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
-
+const express = require('express');
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const path = require('path');
 
-app.use(express.static("public"));
+const PORT = process.env.PORT || 3000;
 
-io.on("connection", (socket) => {
-  socket.on("join", (roomId) => {
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/call/:id', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'call.html'));
+});
+
+io.on('connection', (socket) => {
+  socket.on('join', (roomId) => {
     socket.join(roomId);
-    socket.to(roomId).emit("user-connected", socket.id);
+    socket.to(roomId).emit('user-connected');
 
-    socket.on("signal", (data) => {
-      socket.to(data.to).emit("signal", {
-        from: socket.id,
-        signal: data.signal,
-      });
+    socket.on('signal', (data) => {
+      socket.to(roomId).emit('signal', data);
     });
 
-    socket.on("disconnect", () => {
-      socket.to(roomId).emit("user-disconnected", socket.id);
+    socket.on('disconnect', () => {
+      socket.to(roomId).emit('user-disconnected');
     });
   });
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+http.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
